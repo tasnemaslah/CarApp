78449537648b39a535b1aca1707ea7f8332edd93
tasneem <tasnem.bd@hotmail.com>
2019-01-10 23:32:16 +0200

    first initial commit

---
+++ src/main/java/com/packt/CarApp/CarAppApplication.java
@@ -0,0 +1,14 @@
+package com.packt.CarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAppApplication.class, args);
+	}
+
+}
+
